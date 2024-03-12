a99cdd242ad013c20c61d87b2aae1299e2982a53
Natan <natanalmeida040@gmail.com>
2024-03-12 12:13:02 -0300

    fiz a dashboard e dexei static , agora manytoone

--- example/app/Http/Controllers/DashboardController.php
+++ example/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Categoria;
+use App\Models\Produtos;
+use DB;
 
 class DashboardController extends Controller
 {
@@ -11,6 +15,66 @@ class DashboardController extends Controller
 
 
         public function index(){
-		return view('admin.dashboard');
+		// retornar a quantidade de usuarios no sistema 
+
+		$usuarios = User::all()->count();
+
+
+		// gráfico 1 - usuários   
+
+		$usersData = User::select([
+			DB::raw('YEAR(created_at) as ano'),
+			DB::raw('COUNT(*) as total')
+		])
+		->groupBy('ano')
+		->orderBy('ano', 'asc')
+		->get();
+
+
+
+
+		// arrays de users
+
+		foreach($usersData as $user){
+			 $ano[] = $user->ano;
+			 $total[] = $user->total;
+		}
+	
+
+		// formatar para chartjs
+
+
+
+		$userLabel = "'comparativo de cadastros de usuários'";
+		$userAno = implode(',', $ano); 
+		$userTotal = implode(',',$total);
+
+	//	dd($usersData);
+	
+
+
+
+	// grafico 2  -categorias 
+
+
+	$catData = Categoria::all();
+
+
+	// preparar o array 
+
+		foreach($catData as $cat){
+				$catNome[] = "'".$cat->nome."'";
+				$catTotal[] = Produtos::where('id_categoria', $cat->id)->count();
+
+		}
+
+
+		// formatar para chartjs 
+
+		
+		$catLabel = implode(',', $catNome);
+		$catTotal = implode(',', $catTotal);
+
+			return view('admin.dashboard' , compact('usuarios', 'userLabel','userAno','userTotal', 'catLabel', 'catTotal'));
 	}
 }
--- example/resources/views/admin/dashboard.blade.php
+++ example/resources/views/admin/dashboard.blade.php
@@ -19,7 +19,7 @@
                 <article class="bg-gradient-blue card z-depth-4 ">
                     <i class="material-icons">face</i>
                     <p>Usuários</p>
-                    <h3>35 </h3>
+                    <h3>{{$usuarios}} </h3>
                 </article>
             </div>
 
@@ -53,61 +53,63 @@
 
     </div>
 
+
+
+@endsection
+
 @push('graficos')
 <script>
     /* Gráfico 01 */
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['{ $userAno }'],
-            datasets: [{
-                label: ["{ $userLabel }"],
-                data: [{"{ $userTotal }"}],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [{{ $userAno}}],
+        datasets: [{
+            label: [{!! $userLabel !!}],
+            data:[{{ $userTotal }}],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',                         
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',                     
+                'rgba(255, 159, 64, 1)'
+            ],
+           borderWidth: 1, 
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
         }
-    });
+    }
+});
 
-    /* Gráfico 02 */
-    var ctx = document.getElementById('myChart2');
-    var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: [{ $catLabel }],
-            datasets: [{
-                label: 'Visitas',
-                data: [{"{ $catTotal }""}],
-                backgroundColor: [
-                    'rgba(255, 99, 132)',
-                    'rgba(54, 162, 235)',
-                    'rgba(255, 159, 64)'
-                ]
-            }]
-        }
-    });
+/* Gráfico 02 */
+var ctx = document.getElementById('myChart2');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: [{!! $catLabel !!}],
+        datasets: [{
+            label: 'Visitas',
+            data: [{{$catTotal}}],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',                         
+                'rgba(255, 159, 64)'
+            ]
+        }]
+    }
+});
 </script>
 @endpush
-
-@endsection
